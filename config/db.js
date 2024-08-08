5ac6e959b8cf339b8ba446149b04df501df69347
SebastienLaurentt <slaurent.26@gmail.com>
2024-08-08 11:52:57 +0200

    create admin logs

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-// const userModel = require('../models/user.model');
+const userModel = require('../models/user.model');
 require('dotenv').config({ path: './.env' });
 
 mongoose.set('strictQuery', true);
@@ -11,27 +11,27 @@ mongoose.connect(process.env.MONGODB_URL, {
 })
 .then(() => {
   console.log('Connected to MongoDB');
-  // createAdminUser();
+  createAdminUser();
 })
 .catch(err => {
   console.error('Could not connect to MongoDB', err);
 });
 
-// const createAdminUser = async () => {
-//   try {
-//     const username = process.env.ADMIN_USERNAME || 'admin';
-//     const password = process.env.ADMIN_PASSWORD || 'adminpassword';
+const createAdminUser = async () => {
+  try {
+    const username = process.env.ADMIN_USERNAME || 'admin';
+    const password = process.env.ADMIN_PASSWORD || 'adminpassword';
 
-//     const existingUser = await userModel.findOne({ username });
-//     if (existingUser) {
-//       console.log('Admin user already exists');
-//       return;
-//     }
+    const existingUser = await userModel.findOne({ username });
+    if (existingUser) {
+      console.log('Admin user already exists');
+      return;
+    }
 
-//     const adminUser = new userModel({ username, password });
-//     await adminUser.save();
-//     console.log('Admin user created successfully');
-//   } catch (error) {
-//     console.error('Error creating admin user:', error);
-//   }
-// };
+    const adminUser = new userModel({ username, password });
+    await adminUser.save();
+    console.log('Admin user created successfully');
+  } catch (error) {
+    console.error('Error creating admin user:', error);
+  }
+};
