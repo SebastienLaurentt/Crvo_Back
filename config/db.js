725846a5bb6e2ca58b5c13b8ffe4ea685d940637
SebastienLaurentt <slaurent.26@gmail.com>
2024-08-09 15:48:24 +0200

    update with member user role

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-
 const mongoose = require('mongoose');
 const userModel = require('../models/user.model');
 require('dotenv').config({ path: './.env' });
@@ -11,27 +10,39 @@ mongoose.connect(process.env.MONGODB_URL, {
 })
 .then(() => {
   console.log('Connected to MongoDB');
-  createAdminUser();
+  createInitialUsers();
 })
 .catch(err => {
   console.error('Could not connect to MongoDB', err);
 });
 
-const createAdminUser = async () => {
+const createInitialUsers = async () => {
   try {
-    const username = process.env.ADMIN_USERNAME || 'admin';
-    const password = process.env.ADMIN_PASSWORD || 'adminpassword';
+    const adminUsername = process.env.ADMIN_USERNAME || 'admin';
+    const adminPassword = process.env.ADMIN_PASSWORD || 'adminpassword';
+    const memberUsername = process.env.MEMBER_USERNAME || 'member';
+    const memberPassword = process.env.MEMBER_PASSWORD || 'memberpassword';
 
-    const existingUser = await userModel.findOne({ username });
-    if (existingUser) {
+
+    const existingAdminUser = await userModel.findOne({ username: adminUsername });
+    if (existingAdminUser) {
       console.log('Admin user already exists');
-      return;
+    } else {
+      const adminUser = new userModel({ username: adminUsername, password: adminPassword, role: 'admin' });
+      await adminUser.save();
+      console.log('Admin user created successfully');
     }
 
-    const adminUser = new userModel({ username, password, role: 'admin' });
-    await adminUser.save();
-    console.log('Admin user created successfully');
+
+    const existingMemberUser = await userModel.findOne({ username: memberUsername });
+    if (existingMemberUser) {
+      console.log('Member user already exists');
+    } else {
+      const memberUser = new userModel({ username: memberUsername, password: memberPassword, role: 'member' });
+      await memberUser.save();
+      console.log('Member user created successfully');
+    }
   } catch (error) {
-    console.error('Error creating admin user:', error);
+    console.error('Error creating initial users:', error);
   }
 };
