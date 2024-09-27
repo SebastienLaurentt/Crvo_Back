4ab5af3ab0f569ae6304afc858ed41ddf02998de
SebastienLaurentt <slaurent.26@gmail.com>
2024-09-28 01:51:37 +0200

    improve cors logs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 // Chargement des variables d'environnement
@@ -15,40 +15,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configuration CORS détaillée
-app.use(cors({
-  origin: '*', // En production, remplacez par l'URL de votre frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: "*", // En production, remplacez par l'URL de votre frontend
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // Gestion explicite des requêtes préliminaires (preflight)
-app.options('*', cors());
+app.options("*", cors());
 
 // Middleware pour ajouter des en-têtes CORS supplémentaires si nécessaire
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
 // Middleware de logging pour le débogage
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  console.log("Headers:", req.headers);
   next();
 });
 
 // Routes
-app.use("/", require("./routes/completedVehicule.routes")); 
-app.use("/", require("./routes/vehicle.routes")); 
-app.use("/", require("./routes/user.routes")); 
-app.use("/", require("./routes/cleanUpVehicle.routes")); 
+app.use("/", require("./routes/completedVehicule.routes"));
+app.use("/", require("./routes/vehicle.routes"));
+app.use("/", require("./routes/user.routes"));
+app.use("/", require("./routes/cleanUpVehicle.routes"));
 app.use("/", require("./routes/cleanUpCompletedVehicle.routes"));
 
 // Middleware de gestion des erreurs
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
 });
 
 // Démarrage du serveur
