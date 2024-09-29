a86d7245656ab9e2f0a003c63a958054657cc4a9
SebastienLaurentt <slaurent.26@gmail.com>
2024-09-30 00:50:45 +0200

    remove testController import

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const testController = require("./controllers/test.controller");
 const vehicleController = require("./controllers/vehicle.controller");
 
 require("dotenv").config({ path: "./.env" });
@@ -24,19 +23,13 @@ app.use("/", require("./routes/cleanUpCompletedVehicle.routes"));
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 
-  // Initialise les données de test après le démarrage du serveur
-  // testController
-  //   .initializeTestData()
-  //   .then(() => console.log("Initialisation des données de test terminée"))
-  //   .catch((err) =>
-  //     console.error("Erreur lors de l'initialisation des données de test:", err)
-  //   );
-
-  // Initialise les données de véhicules après le démarrage du serveur
   vehicleController
     .initializeVehicleData()
     .then(() => console.log("Initialisation des données de véhicules terminée"))
     .catch((err) =>
-      console.error("Erreur lors de l'initialisation des données de véhicules:", err)
+      console.error(
+        "Erreur lors de l'initialisation des données de véhicules:",
+        err
+      )
     );
 });
