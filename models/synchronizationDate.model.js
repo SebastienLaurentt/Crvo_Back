70025744aa1c26bf0e7035c846139aff48cfe425
SebastienLaurentt <slaurent.26@gmail.com>
2024-10-18 01:26:21 +0200

    remove local date because front handle it

--- models/synchronizationDate.model.js
+++ models/synchronizationDate.model.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
-const { addHours } = require('date-fns');
 
 const synchronizationDateSchema = new mongoose.Schema({
   date: { 
     type: Date, 
-    default: () => {
-      const now = new Date();
-      const parisTime = addHours(now, 2); 
-      return parisTime;
-    }
+    default: Date.now 
   }
 });
 
