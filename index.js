1f5e1085279eecb218f9c2518c1e8d9defaf965b
SebastienLaurentt <slaurent.26@gmail.com>
2024-10-19 17:32:11 +0200

    vehicule data update is now handled with cron on prod

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const vehicleController = require("./controllers/vehicle.controller");
 const bodyParser = require("body-parser");
+const cron = require("node-cron");
 
 require("dotenv").config({ path: "./.env" });
 require("./config/db");
@@ -19,14 +20,16 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 
   const updateVehicleData = async () => {
-    console.log("Lancement de la synchronisation initiale des véhicules");
+    console.log("Lancement de la synchronisation des véhicules");
     try {
       await vehicleController.runVehicleSynchronization();
-      console.log("Synchronisation initiale des véhicules terminée");
+      console.log("Synchronisation des véhicules terminée");
     } catch (err) {
-      console.error("Erreur lors de la synchronisation initiale des véhicules:", err);
+      console.error("Erreur lors de la synchronisation des véhicules:", err);
     }
   };
 
-  updateVehicleData();
+  cron.schedule("5 * * * *", () => {
+    updateVehicleData();
+  });
 });
