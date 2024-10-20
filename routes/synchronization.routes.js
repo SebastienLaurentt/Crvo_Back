a4a65cc0f8f25563b209455e6d1d66223dec00fb
SebastienLaurentt <slaurent.26@gmail.com>
2024-10-20 21:03:09 +0200

    add requireAuth on  getLatestSynchroDate route

--- routes/synchronization.routes.js
+++ routes/synchronization.routes.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getLatestSynchronizationDate } = require("../controllers/synchronization.controller");
+const {
+  getLatestSynchronizationDate,
+} = require("../controllers/synchronization.controller");
+const { requireAuth } = require("../middleware/authMiddleware");
 
-router.get("/api/synchronization", getLatestSynchronizationDate);
+router.get("/api/synchronization", requireAuth, getLatestSynchronizationDate);
 
 module.exports = router;
+ 
