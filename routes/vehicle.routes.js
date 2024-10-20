2c99b59f537bb13ef03affd50a7b495dca583c1e
SebastienLaurentt <slaurent.26@gmail.com>
2024-10-20 20:42:21 +0200

    add requireAuth on  getAllVehicules route

--- routes/vehicle.routes.js
+++ routes/vehicle.routes.js
@@ -5,7 +5,7 @@ const {
 const { requireAuth } = require("../middleware/authMiddleware");
 const router = require("express").Router();
 
-router.get("/api/vehicles", getAllVehicles);
+router.get("/api/vehicles", requireAuth, getAllVehicles);
 router.get("/api/user/vehicles", requireAuth, getVehiclesByUser);
 
 module.exports = router;
